97e283801849ac80b6800ba22c0b83bf18dde1db
Garann Means <garann@gmail.com>
2012-03-02 19:15:10 -0600

    styling, screwy javascript

---
+++ fonts/Tup Wanders Font License.txt
@@ -0,0 +1,9 @@
+Free font by Tup Wanders
+http://www.tupwanders.nl
+
+Licensed with a Creative Commons attribution license.
+
+If you add to the font, please send me a copy! If you've made fun stuff with the font that you would like to show me, please send me that as well. I like that.
+
+Have fun,
+Tuppus
---
+++ script.js
@@ -0,0 +1,16 @@
+$( function () {
+	$( "div.criteria" ).on( "click", "fieldset", function() {
+		var $t = $( this );
+		if ( $t.hasClass( "off" ) ) {
+			$t.removeClass( "off" ).addClass( "on" );
+		} else {
+			$t.addClass( "off" ).removeClass( "on" );
+		}
+	});
+	$( "div.criteria" ).on( "click", "label", function( e ) {
+		e.stopPropagation();
+		var cname = $( this ).find( "input" ).val();
+		$( "div.engines div:not(." + cname + ")" ).removeClass( "add" ).addClass( "remove" );
+		$( "div.engines div." + cname ).addClass( "add" ).removeClass( "remove" );
+	});
+});
