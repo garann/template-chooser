52850017d1be58d08a1b4711a821d60d0ef62cbf
Garann Means <garann@gmail.com>
2013-06-05 21:24:28 -0500

    allow toggling criteria on/off - closes #10
    also added nicer CSS syntax, h/t @xinsight

--- script.js
+++ script.js
@@ -111,44 +111,61 @@ var engines = [
 
 $( function () {
 	// render all engines
-	var tmpls = doT.template( $( "#tmplsTmpl" ).text() );
-	$( "div.engines" ).html( tmpls( { all: engines } ) );
+	var tmpls = doT.template( $( "#tmplsTmpl" ).text() ),
+		$eng = $( "div.engines" );
+	$eng.html( tmpls( { all: engines } ) );
 
 	$( "div.criteria" ).on( "click", "fieldset", function() {
 		var $t = $( this );
-		if ( $t.hasClass( "off" ) ) {
-			$t.removeClass( "off" ).addClass( "on" );
+		if ( $t.hasClass( "on" ) ) {
+			$t.removeClass( "on" );
+			removeSibs( $t.find( "label" ) );
+			updateClasses();
 		} else {
-			$t.addClass( "off" ).removeClass( "on" );
+			$t.addClass( "on" );
+			var curr = $t.find( "input:checked" );
+			if ( curr.length ) {
+				setCriteria( curr.parent() );
+			}
 		}
 	});
 	$( "div.criteria" ).on( "click", "label", function( e ) {
 		e.stopPropagation();
-		var $t = $( this ),
+		setCriteria( this );
+	});
+
+	function setCriteria( lbl ) {
+		var $t = $( lbl ),
 			cname = $t.find( "input" ).val(),
-			sibs = $t.siblings( "label" ),
-			removeIndex,
 			classes;
+
+		removeSibs( $t.siblings( "label" ) );
+		if ( cname.length ) {
+			$.each( cname.split( "." ), function( i, nm ) {
+				if ( chooser.choices.indexOf( nm ) === -1 ) {
+					chooser.choices.push( nm );
+				}
+			});
+		}
+		updateClasses();
+	}
+
+	function removeSibs( sibs ) {
+		var removeIndex;
+
 		$( sibs ).each( function() {
 			removeIndex = chooser.choices.indexOf( $( this ).find( "input" ).val() );
 			if ( removeIndex > -1 ) chooser.choices.splice( removeIndex, 1 );
 		});
-		if ( cname.length ) {
-			if ( cname.indexOf( "." ) > -1 ) {
-				$.each( cname.split( "." ), function( i, nm ) {
-					if ( chooser.choices.indexOf( nm ) === -1 ) {
-						chooser.choices.push( nm );
-					}
-				});
-			} else {
-				if ( chooser.choices.indexOf( cname ) === -1 ) {
-					chooser.choices.push( cname );
-				}
-			}
+	}
+
+	function updateClasses() {
+		if ( !chooser.choices.length ) {
+			$eng.find( "div.remove" ).addClass( "add" ).removeClass( "remove" );
+			return;
 		}
-		if ( !chooser.choices.length ) return;
 		classes = chooser.choices.join( "." );
-		$( "div.engines div:not(." + classes + ")" ).addClass( "remove" ).removeClass( "add" );
-		$( "div.engines div." + classes ).addClass( "add" ).removeClass( "remove" );
-	});
+		$eng.find( "div:not(." + classes + ")" ).addClass( "remove" ).removeClass( "add" );
+		$eng.find( "div." + classes ).addClass( "add" ).removeClass( "remove" );
+	}
 });
